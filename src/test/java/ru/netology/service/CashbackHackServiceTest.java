c9426103e490db9f457946700c462f0f738ce000
TregubovAV <5595@rambler.ru>
2024-10-09 11:39:10 +0300

    Initial commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,70 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void chashback() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 2000;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void chashback1() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 999;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1;
+
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void chashback2() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+
+    }
+
+
+    @Test
+    public void chashback3() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 999;
+
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void chashback4() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1001;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 999;
+
+        assertEquals(actual, expected);
+
+    }
+
+}
